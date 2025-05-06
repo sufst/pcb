2851a7f096e3d50b815d8cd46b07c3408b9abf79
Martin <maartin00000@gmail.com>
2025-05-06 18:39:32 +0100

    Feat/tidy actions (#138)
    * fix(actions): Overwrite existing tag numbers rather than erroring when generating production files
    
    * move scripts from ./bin to github.com/sufst/sufst-scripts
    
    * Only test changes on [push] rather than [push, pull_request] since pull requests will be checked anyway when changes are added
    
    * Generate drill files as well as gerbers
    
    * refactor actions to use composite container actions which makes everything 3x faster and more modular (so easier to read)
    
    * edit initial suggestion message if PR is updated

--- bin/util/copy-lib-tables.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# run kicad-cli once to generate .config/kicad/<version> then copy default lib tables
-kicad-cli -v
-
-for version in ~/.config/kicad/*; do 
-    cp /usr/share/kicad/template/*-lib-table "$version/"
-done
